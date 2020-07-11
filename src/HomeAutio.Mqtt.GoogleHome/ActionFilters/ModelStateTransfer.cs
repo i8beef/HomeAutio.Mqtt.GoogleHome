d7b9fbbe2bfadb39a2b19dfda57aa7341e180ee8
Michael Hallock <mike@hallock.net>
2020-07-11 15:14:21 -0400

    Adds a workaround for possible modelstate deserialization issue

--- src/HomeAutio.Mqtt.GoogleHome/ActionFilters/ModelStateTransfer.cs
+++ src/HomeAutio.Mqtt.GoogleHome/ActionFilters/ModelStateTransfer.cs
@@ -47,7 +47,10 @@ namespace HomeAutio.Mqtt.GoogleHome.ActionFilters
 
             foreach (var item in errorList)
             {
-                modelState.SetModelValue(item.Key, item.RawValue, item.AttemptedValue);
+                var array = item.RawValue as Newtonsoft.Json.Linq.JArray;
+                var value = array?.ToObject<string[]>() ?? item.RawValue;
+
+                modelState.SetModelValue(item.Key, value, item.AttemptedValue);
                 foreach (var error in item.ErrorMessages)
                 {
                     modelState.AddModelError(item.Key, error);
