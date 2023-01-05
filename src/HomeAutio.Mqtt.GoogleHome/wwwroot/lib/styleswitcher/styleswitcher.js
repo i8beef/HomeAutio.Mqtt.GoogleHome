68912a675afcd731bfa204b9328a37a09b1611ca
Michael Hallock <mike@hallock.net>
2023-01-05 00:25:52 -0500

    Move to a CSS overide path instead of attempting to use all the half-broken bootstrap theming tools out there

--- src/HomeAutio.Mqtt.GoogleHome/wwwroot/lib/styleswitcher/styleswitcher.js
+++ src/HomeAutio.Mqtt.GoogleHome/wwwroot/lib/styleswitcher/styleswitcher.js
@@ -1,9 +1,11 @@
-﻿function setActiveStyleSheet(title) {
+function setActiveStyleSheet(title) {
     var i, a, main;
     for (i = 0; (a = document.getElementsByTagName("link")[i]); i++) {
         if (a.getAttribute("rel").indexOf("style") != -1 && a.getAttribute("title")) {
             a.disabled = true;
-            if (a.getAttribute("title") == title) a.disabled = false;
+            if (a.getAttribute("title") == title) {
+              a.disabled = false;
+            }
         }
     }
 }
