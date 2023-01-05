6d505219d816b3528dce78569524620f530a4712
Michael Hallock <mike@hallock.net>
2023-01-05 03:30:24 -0500

    Modifies some content security policy settings

--- src/IdentityServerHost.Quickstart.UI/SecurityHeadersAttribute.cs
+++ src/IdentityServerHost.Quickstart.UI/SecurityHeadersAttribute.cs
@@ -27,7 +27,10 @@ namespace IdentityServerHost.Quickstart.UI
                 }
 
                 // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
-                var csp = "default-src 'self'; object-src 'none'; frame-ancestors 'none'; sandbox allow-forms allow-same-origin allow-scripts; base-uri 'self';";
+                //var csp = "default-src 'self'; object-src 'none'; frame-ancestors 'none'; sandbox allow-forms allow-same-origin allow-scripts; base-uri 'self';";
+
+                // The above is a bunch of bullshit, open it up
+                var csp = "default-src 'self'; img-src 'self' data:; style-src 'self' fonts.googleapis.com; font-src 'self' fonts.gstatic.com; object-src 'none'; frame-ancestors 'none'; sandbox allow-forms allow-same-origin allow-scripts; base-uri 'self';";
                 // also consider adding upgrade-insecure-requests once you have HTTPS in place for production
                 //csp += "upgrade-insecure-requests;";
                 // also an example if you need client images to be displayed from twitter
